3cf671b843d104c7ef51dcd2d937b9bf0168e155
Umais Khan <omaisk.me42ceme@student.nust.edu.pk>
2024-08-03 20:04:51 +0500

    app end

--- Flask/hello/app.py
+++ Flask/hello/app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
 
 app=Flask(__name__)
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 
 def index():
+    if request.method=="POST":
+        name=request.form.get("name","world")
+        return render_template("greet.html", name=name)
+    else:
+        return render_template("index.html")
+    #return render_template("index.html")   # using render templates
 
-    return render_template("index.html")   # using render templates
-
-@app.route("/greet",methods=["GET","POST"])  # add methods 
-def greet():
-    name=request.args.get("name","world")
-    return render_template("greet.html", name=name)
+# @app.route("/greet",methods=["GET","POST"])  # add methods 
+# def greet():
+#     name=request.args.get("name","world")
+#     return render_template("greet.html", name=name)
