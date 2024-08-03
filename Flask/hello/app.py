33adaea2a47e18ca9f2bf2379f112616a7c7ab86
Umais Khan <omaisk.me42ceme@student.nust.edu.pk>
2024-08-03 19:50:03 +0500

    get method

--- Flask/hello/app.py
+++ Flask/hello/app.py
@@ -7,7 +7,7 @@ def index():
 
     return render_template("index.html")   # using render templates
 
-@app.route("/greet")
+@app.route("/greet",methods=["GET","POST"])  # add methods 
 def greet():
     name=request.args.get("name","world")
     return render_template("greet.html", name=name)
